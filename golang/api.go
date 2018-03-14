0310e2196d4c99e5ce2192364c51fcf75d73d018
sisisytze <s.tempel@gmail.com>
2018-03-14 14:50:43 +0100

    more text/explenations for api

--- golang/api.go
+++ golang/api.go
@@ -16,19 +16,19 @@ import (
 )
 
 var (
-	sensors []Sensor
 	measurements map[time.Time][]Measurement //Used to cache the data.
 	databases [][2]sql.Database // We have 2 databases wit duplicate data. Store one of each. If [x][0] fails use [x][1] (later we could base this on loadbalancers??)
 )
 
 func main() {
 	var (
+	  databasesLocation string
 		port string
 		cacheTimeout time.Duration
 	)
+	flag.StringVar(&databasesLocation, "", "file name and location with the database addresses")
 	flag.StringVar(&port, "p", ":8080", "ip:port to listen to")
 	flag.Duration(&cacheTimeout, time.ParseDuration("5m"), "time for measurements to stay in cache, should be entered as a number followed by m for minutes h for hours: 5m or 5h")
-
 	flag.Parse()
 
 	db, err := sql.Open("mysql", dsn)
@@ -37,11 +37,12 @@ func main() {
 	}
 
 	initialize()
-	connectDatabases()
+	connectDatabases(databasesLocation)
 
 	db.SetMacIdleConns(0)
 
-	http.HandleFunc("/api/get/sensor", getSensor)
+	// HandlerFunc is a http functions. If there is a request at our base url + "/api/get/sensor" a new go routine running the function getMeasurements(..)
+	http.HandlerFunc("/api/get/measurements", getMeasurements)
 	
 	server := &http.Server{Addr: ":8080"}
 	server.ListenAndServe()
@@ -52,14 +53,16 @@ func initialize() {
 	go garbageCollectorCache(ctx context.ContextWithCancel(context.Background))
 }
 
-funct connectDatabases() {
+funct connectDatabases(location string) {
   // load in file with all the databases (in the file the duplicates should be paired)
 	// connect to all databases en put them in as globals
 }
 
-
+/*
+ * are we going to do anything with securtity??
+ */
 func getMeasurements(w http.ResponseWriter, r *http.Request) {
-	// get time from request
+	// get time from request querry the request parameters for time
 	// see if measurements[time] exists
 		 // if exists return those measurements as json
 		 // if not querry (all) database(s)
